1acdbd4f637e86379870393ab48bb3b99bd62e5a
MP <markpaololibunao@gmail.com>
2022-09-08 17:03:46 +0800

    Stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,16 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 const Home: NextPage = () => {
-	return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+	return (
+		<div className='flex-col justify-center w-screen h-screen flex items-center'>
+			<div className='text-2xl'>Which Pokemon is rounder?</div>
+			<div className='border rounded p-8 flex justify-between max-w-2xl'>
+				<div className='w-16 h-16 bg-red-200' />
+				<div>Vs</div>
+				<div className='w-16 h-16 bg-red-200' />
+			</div>
+		</div>
+	);
 };
 
 export default Home;

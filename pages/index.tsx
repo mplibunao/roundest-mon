58baea838804e1c9d0710094d5a82a60ecb17131
MP <markpaololibunao@gmail.com>
2022-10-05 15:12:45 +0800

    Basic ui structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,19 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
 	return (
 		<div className='flex-col justify-center w-screen h-screen flex items-center'>
 			<div className='text-2xl'>Which Pokemon is rounder?</div>
-			<div className='border rounded p-8 flex justify-between max-w-2xl'>
+			<div className='p-2' />
+			<div className='border rounded p-8 flex justify-between max-w-2xl items-center'>
 				<div className='w-16 h-16 bg-red-200' />
-				<div>Vs</div>
+				<div className='p-8'>Vs</div>
 				<div className='w-16 h-16 bg-red-200' />
 			</div>
 		</div>
-	);
-};
+	)
+}
 
-export default Home;
+export default Home

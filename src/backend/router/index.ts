f112061bccfb2c12fa1d5faf0b01871917a0ac1a
MP <markpaololibunao@gmail.com>
2022-10-06 14:46:52 +0800

    Uses package to get pokemon data and render sprite

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,17 +1,16 @@
 import * as trpc from '@trpc/server'
+import { PokemonClient } from 'pokenode-ts'
 
 import { z } from 'zod'
 
-export const appRouter = trpc.router().query('hello', {
-	input: z
-		.object({
-			text: z.string().nullish(),
-		})
-		.nullish(),
-	resolve({ input }) {
-		return {
-			greeting: `hello ${input?.text ?? 'world'}`,
-		}
+export const appRouter = trpc.router().query('get-pokemon-by-id', {
+	input: z.object({
+		id: z.number(),
+	}),
+	async resolve({ input }) {
+		const api = new PokemonClient()
+		const pokemon = await api.getPokemonById(input.id)
+		return pokemon
 	},
 })
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,40 @@ const Home: NextPage = () => {
 	React.useEffect(() => {
 		setIds(getOptionsForVote())
 	}, [])
+
+	const firstPokemon = trpc.useQuery(['get-pokemon-by-id', { id: first }])
+	const secondPokemon = trpc.useQuery(['get-pokemon-by-id', { id: second }])
+
+	if (firstPokemon.isLoading || secondPokemon.isLoading) {
+		return null
+	}
+
+	console.log('firstPokemon', firstPokemon) // eslint-disable-line no-console
 	return (
 		<div className='flex-col justify-center w-screen h-screen flex items-center'>
 			<div className='text-2xl'>Which Pokemon is rounder?</div>
 			<div className='p-2' />
 			<div className='border rounded p-8 flex justify-between max-w-2xl items-center'>
-				<div className='w-16 h-16 bg-red-800'>{first}</div>
+				<div className='w-64 h-64 flex flex-col'>
+					<img
+						src={firstPokemon.data?.sprites.front_default}
+						className='w-full'
+					/>
+					<div className='capitalize text-xl text-center mt-[-2rem]'>
+						{firstPokemon.data?.name}
+					</div>
+				</div>
 				<div className='p-8'>Vs</div>
-				<div className='w-16 h-16 bg-red-800'>{second}</div>
+				<div className='w-64 h-64 flex flex-col'>
+					<img
+						src={secondPokemon.data?.sprites.front_default}
+						className='w-full'
+					/>
+					<div className='capitalize text-xl text-center mt-[-2rem]'>
+						{secondPokemon.data?.name}
+					</div>
+				</div>
+				<div className='p-2' />
 			</div>
 		</div>
 	)

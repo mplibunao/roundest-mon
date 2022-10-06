bd0fe86ffe63c6d152a45e62fa82dce036be203f
MP <markpaololibunao@gmail.com>
2022-10-06 14:09:57 +0800

    Adds random pokemon id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,22 @@
+import { getOptionsForVote } from '@/utils/getRandomPokemon'
 import { trpc } from '@/utils/trpc'
 import type { NextPage } from 'next'
+import React from 'react'
 
 const Home: NextPage = () => {
-	const { data, isLoading } = trpc.useQuery(['hello', { text: 'MP' }])
+	const [[first, second], setIds] = React.useState<number[]>([0, 0])
 
-	if (isLoading) return <div>loading...</div>
-
-	if (data) return <div>{data.greeting}</div>
-
+	React.useEffect(() => {
+		setIds(getOptionsForVote())
+	}, [])
 	return (
 		<div className='flex-col justify-center w-screen h-screen flex items-center'>
 			<div className='text-2xl'>Which Pokemon is rounder?</div>
 			<div className='p-2' />
 			<div className='border rounded p-8 flex justify-between max-w-2xl items-center'>
-				<div className='w-16 h-16 bg-red-200' />
+				<div className='w-16 h-16 bg-red-800'>{first}</div>
 				<div className='p-8'>Vs</div>
-				<div className='w-16 h-16 bg-red-200' />
+				<div className='w-16 h-16 bg-red-800'>{second}</div>
 			</div>
 		</div>
 	)

9302d374e7e4c030a9bc617c030a30039eb6a9b0
MP <markpaololibunao@gmail.com>
2022-10-08 19:49:50 +0800

    Improves client name

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -11,8 +11,8 @@ export const appRouter = trpc
 			id: z.number(),
 		}),
 		async resolve({ input }) {
-			const api = new PokemonClient()
-			const pokemon = await api.getPokemonById(input.id)
+			const pokeApiConnection = new PokemonClient()
+			const pokemon = await pokeApiConnection.getPokemonById(input.id)
 			return { name: pokemon.name, sprites: pokemon.sprites }
 		},
 	})

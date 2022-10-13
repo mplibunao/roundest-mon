70049fdc6e21cc9d10ec50b5351ead70813598fa
MP <markpaololibunao@gmail.com>
2022-10-13 13:38:45 +0800

    Improves results pages look and increases revalidate time

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from 'next'
 import { prisma } from '@/backend/utils/prisma'
 import { AsyncReturnType } from '@/utils/ts-bs'
 import Image from 'next/image'
+import SEO from '@/components/SEO'
 
 type PokemonQueryResult = AsyncReturnType<typeof getPokemonInOrder>
 
@@ -13,23 +14,35 @@ export default function ResultsPage({
 	pokemon,
 }: ResultsPageProps): JSX.Element {
 	return (
-		<div className='flex flex-col items-center'>
-			<h2 className='text-2xl p-4'>ResultsPage</h2>
 		<>
 			<SEO
+				title='Results'
+				description='View the voting results on which pokemon is roundest'
+			/>
+			<div className='flex flex-col items-center'>
+				<h2 className='text-2xl p-4'>ResultsPage</h2>
 
-			<div className='flex flex-col max-w-2xl w-full border'>
-				{pokemon
-					.sort((a, b) => generateCountPercent(b) - generateCountPercent(a))
-					.map((currentPokemon, rank) => (
-						<PokemonListing
-							pokemon={currentPokemon}
-							key={currentPokemon.id}
-							rank={rank + 1}
-						/>
-					))}
+				<div className='flex flex-col max-w-2xl w-full border'>
+					{pokemon
+						.sort((a, b) => {
+							const difference =
+								generateCountPercent(b) - generateCountPercent(a)
+
+							if (difference === 0) {
+								return b._count.voteFor - a._count.voteFor
+							}
+
+							return difference
+						})
+						.map((currentPokemon, rank) => (
+							<PokemonListing
+								pokemon={currentPokemon}
+								key={currentPokemon.id}
+								rank={rank + 1}
+							/>
+						))}
+				</div>
 			</div>
-		</div>
 		</>
 	)
 }
@@ -56,7 +69,7 @@ export const getStaticProps: GetStaticProps<ResultsPageProps> = async () => {
 
 	return {
 		props: { pokemon: pokemonOrdered },
-		revalidate: 60,
+		revalidate: 60 * 60 * 24,
 	}
 }
 
@@ -67,20 +80,25 @@ interface PokemonListingProps {
 
 const PokemonListing = ({ pokemon, rank }: PokemonListingProps) => {
 	return (
-		<div className='flex border-b p-2 items-center justify-between'>
+		<div className='relative flex border-b p-2 items-center justify-between'>
 			<div className='flex items-center'>
-				<Image
-					src={pokemon.spriteUrl}
-					width={64}
-					height={64}
-					alt={`${pokemon.name} is rank ${rank} in roundness`}
-					layout='fixed'
-				/>
-				<div className='capitalize'>{pokemon.name}</div>
+				<div className='flex items-center pl-4'>
+					<Image
+						src={pokemon.spriteUrl}
+						width={64}
+						height={64}
+						alt={`${pokemon.name} is rank ${rank} in roundness`}
+						layout='fixed'
+					/>
+					<div className='pl-2 capitalize'>{pokemon.name}</div>
+				</div>
 			</div>
-			<div className='pr-4'>{`${generateCountPercent(pokemon).toFixed(
-				2
-			)}%`}</div>
+			<div className='pr-4'>
+				{`${generateCountPercent(pokemon).toFixed(2)}%`}
+			</div>
+			<div className='absolute top-0 left-0 z-20 flex items-center justify-center px-2 font-semibold text-white bg-gray-600 border border-gray-500 shadow-lg rounded-br-md'>
+				{rank}
+			</div>
 		</div>
 	)
 }
